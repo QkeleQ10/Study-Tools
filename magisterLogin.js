b19cf07044092175a11059fbe0593e6cc9f2e3f2
QkeleQ10 <quintenra@gmail.com>
2021-06-01 11:10:43 +0200

    r

--- magisterLogin.js
+++ magisterLogin.js
@@ -1,31 +1,41 @@
-// ✍️ Je kunt de volgende opties aanpassen.
-
-let gebruikersnaam = "" // Je gebruikersnaam voor Magister.
-let wachtwoord = "" // Je wachtwoord voor Magister.
-
-
-
-// 🙅 Verander hieronder niks.
-
 checkUpdates()
 login()
 
 async function login() {
 
+    let mouseDown = 0
+    document.body.onmousedown = function () { ++mouseDown }
+    document.body.onmouseup = function () { --mouseDown }
+
+    let qstaUsername = document.cookie.split("qstaUsername=")[1]
+    if (qstaUsername) qstaUsername = qstaUsername.split(";")[0]
+
+    let qstaPassword = document.cookie.split("qstaPassword=")[1]
+    if (qstaPassword) qstaPassword = qstaPassword.split(";")[0]
+
+    document.querySelector(".bottom").outerHTML = `
+    <div class="bottom" style="justify-content: space-between; align-items: center">
+        Klik of laat los om te annuleren.
+        <input type='text' placeholder='Autologin gebruikersnaam' onkeyup='document.cookie = "qstaUsername=" + this.value; console.log(this.value)' value=${qstaUsername}>
+        <input type='password' placeholder='Autologin wachtwoord' onkeyup='document.cookie = "qstaPassword=" + this.value; console.log(this.value)' value=${qstaPassword}>
+    </div>`
+
     await awaitElement("#username")
-    if (!gebruikersnaam) return document.querySelector(".bottom").innerHTML = "Je kunt automatisch invoeren instellen met de opties in magisterLogin.js."
-    document.getElementById("username").value = gebruikersnaam
+    if (mouseDown != 0) return
+    document.getElementById("username").value = qstaUsername || ""
     document.getElementById("username").dispatchEvent(new Event("input"))
 
     await awaitElement("#username_submit")
+    if (mouseDown != 0) return
     document.getElementById("username_submit").click()
 
     await awaitElement("#rswp_password")
-    if (!wachtwoord) return document.querySelector(".bottom").innerHTML = "Je kunt automatisch invoeren instellen met de opties in magisterLogin.js."
-    document.getElementById("rswp_password").value = wachtwoord
+    if (mouseDown != 0) return
+    document.getElementById("rswp_password").value = qstaPassword || ""
     document.getElementById("rswp_password").dispatchEvent(new Event("input"))
 
     await awaitElement("[id*=_submit]")
+    if (mouseDown != 0) return
     document.getElementById("rswp_submit").click()
 
 }
