2998ad835f414dd3a62c969fc5ba77ef96d0e557
QkeleQ10 <quintenra@gmail.com>
2021-06-18 10:35:32 +0200

    feat: more polish

--- magisterLogin.js
+++ magisterLogin.js
@@ -3,9 +3,9 @@ login()
 
 async function login() {
 
-    let mouseDown = 0
-    document.body.onmousedown = function () { ++mouseDown }
-    document.body.onmouseup = function () { --mouseDown }
+    let keysChanged = false
+    document.body.onkeydown = function () { keysChanged = true }
+    document.body.onkeyup = function () { keysChanged = true }
 
     let qstaUsername = document.cookie.split("qstaUsername=")[1]
     if (qstaUsername) qstaUsername = qstaUsername.split(";")[0]
@@ -15,27 +15,27 @@ async function login() {
 
     document.querySelector(".bottom").outerHTML = `
     <div class="bottom" style="justify-content: space-between; align-items: center">
-        Klik of laat los om te annuleren.
-        <input type='text' placeholder='Autologin gebruikersnaam' onkeyup='document.cookie = "qstaUsername=" + this.value + ";expires=Fri, 31 Dec 9999 23:59:59 GMT;"; console.log(this.value)' value=${qstaUsername | ""}>
-        <input type='password' placeholder='Autologin wachtwoord' onkeyup='document.cookie = "qstaPassword=" + this.value";expires=Fri, 31 Dec 9999 23:59:59 GMT;"; console.log(this.value)' value=${qstaPassword | ""}>
+        Druk op een toets om te annuleren.
+        <input type='text' placeholder='Autologin gebruikersnaam' oninput='document.cookie = "qstaUsername=" + this.value + ";expires=Fri, 31 Dec 9999 23:59:59 GMT;"; console.log(this.value)' ${qstaUsername ? "value=" + qstaUsername : ""}>
+        <input type='password' placeholder='Autologin wachtwoord' oninput='document.cookie = "qstaPassword=" + this.value + ";expires=Fri, 31 Dec 9999 23:59:59 GMT;"; console.log(this.value)' ${qstaPassword ? "value=" + qstaPassword : ""}>
     </div>`
 
     await awaitElement("#username")
-    if (mouseDown != 0) return
+    if (keysChanged) return
     document.getElementById("username").value = qstaUsername || ""
     document.getElementById("username").dispatchEvent(new Event("input"))
 
     await awaitElement("#username_submit")
-    if (mouseDown != 0) return
+    if (keysChanged) return
     document.getElementById("username_submit").click()
 
     await awaitElement("#rswp_password")
-    if (mouseDown != 0) return
+    if (keysChanged) return
     document.getElementById("rswp_password").value = qstaPassword || ""
     document.getElementById("rswp_password").dispatchEvent(new Event("input"))
 
     await awaitElement("[id*=_submit]")
-    if (mouseDown != 0) return
+    if (keysChanged) return
     document.getElementById("rswp_submit").click()
 
 }
