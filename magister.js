2a45b460cc46aea340d6f98bcc2cb3d30aedc290
QkeleQ10 <quinten@althues.nl>
2022-11-20 23:23:10 +0100

    Studiewijzers in raster & verbeterde ordenfunctie

--- magister.js
+++ magister.js
@@ -18,34 +18,88 @@ async function agenda() {
 
 async function studiewijzers() {
     if (await getSetting('magister-sw-sort')) {
-        let swList = await getElement('.studiewijzer-list>ul'),
-            swNodeList = await getElement('li[data-ng-repeat="studiewijzer in items"]', true)
+        let elementMain = await getElement('section.main'),
+            elementOldContainer = await getElement('.content-container'),
+            elementGrid = document.createElement('div'),
+            elementUl = await getElement('.studiewijzer-list>ul'),
+            settingGrid = await getSetting('magister-sw-grid'),
+            settingSubjects = await getSetting('magister-sw-subjects'),
+            nodeList = await getElement('li[data-ng-repeat="studiewijzer in items"]', true),
+            nodeArray = [...nodeList],
+            mappedArray = nodeArray.map(elem => {
+                let swTitle = elem.firstElementChild.firstElementChild,
+                    swSubject = elem.firstElementChild.lastElementChild,
+                    subject,
+                    period,
+                    priority,
+                    periodTextIndex = swTitle.innerText.search(/(t(hema)?|p(eriod(e)?)?)(\s|\d)/i)
 
-        swNodeList.forEach(async elem => {
-            let swTitle = elem.firstElementChild.firstElementChild,
-                swSubject = elem.firstElementChild.lastElementChild,
-                period,
-                periodTextIndex = swTitle.innerText.search(/(t(hema)?|p(eriod(e)?)?)(\s|\d)/i)
-            if (periodTextIndex > 0) {
-                let periodNumberSearchString = swTitle.innerText.slice(periodTextIndex),
-                    periodNumberIndex = periodNumberSearchString.search(/[1-9]/i)
-                if (periodNumberIndex > 0) {
-                    period = Number(periodNumberSearchString.charAt(periodNumberIndex))
+                Object.values(settingSubjects)[0].forEach(names => {
+                    names.forEach(name => {
+                        if ((new RegExp(`^(${name.trim()})$|^(${name.trim()})[^a-z]|[^a-z](${name.trim()})$|[^a-z](${name.trim()})[^a-z]`, 'i')).test(swTitle.innerText)) subject = names[0]
+                    })
+                })
+
+                if (periodTextIndex > 0) {
+                    let periodNumberSearchString = swTitle.innerText.slice(periodTextIndex),
+                        periodNumberIndex = periodNumberSearchString.search(/[1-9]/i)
+                    if (periodNumberIndex > 0) {
+                        period = Number(periodNumberSearchString.charAt(periodNumberIndex))
+                    }
                 }
-            }
 
-            if (period == await getPeriodNumber(await getWeekNumber())) {
-                elem.classList.add('st-current')
-                elem.setAttribute('title', "Deze studiewijzer is actueel.")
-                swList.prepend(elem)
-            } else if (period > 0) {
-                elem.classList.add('st-obsolete')
-                elem.setAttribute('title', `Deze studiewijzer is van periode ${period}.`)
-                swList.appendChild(elem)
-            } else {
-                elem.setAttribute('title', "Er kon geen periodenummer worden gedetecteerd.")
+                if (period === getPeriodNumber()) priority = 2
+                else if (period > 0) priority = 0
+                else priority = 1
+
+                return { elem: elem, title: swTitle.innerText, period: period, subject: subject || "Geen vak", priority: priority }
+            }).sort((a, b) => (b.priority - a.priority || a.subject.localeCompare(b.subject)))
+
+        if (settingGrid) {
+            elementOldContainer.style.display = 'none'
+            elementMain.appendChild(elementGrid)
+            elementGrid.classList.add('st-sw-grid')
+        }
+
+        mappedArray.forEach(({ elem, title, period, subject, priority }) => {
+            elementUl.appendChild(elem)
+            elem.firstElementChild.lastElementChild.innerText = subject
+            switch (priority) {
+                case 2:
+                    if (settingGrid) {
+                        let button = document.createElement('button')
+                        elementGrid.appendChild(button)
+                        button.classList.add('st-sw-item', 'st-current')
+                        button.innerHTML = `<h3>${subject}</h3><p style="font-weight:bold">periode ${period}</p><p style="font-weight:normal;font-size:10px;color:#808080">${title}</p>`
+                        button.addEventListener('click', () => { elem.firstElementChild.click() })
+                    }
+                    elem.classList.add('st-current')
+                    elem.setAttribute('title', "Deze studiewijzer is actueel.")
+                    break
+
+                case 1:
+                    if (settingGrid) {
+                        let button = document.createElement('button')
+                        elementGrid.appendChild(button)
+                        button.classList.add('st-sw-item')
+                        button.innerHTML = `<h3>${subject}</h3><p style="font-weight:bold">geen periode</p><p style="font-weight:normal;font-size:10px;color:#808080">${title}</p>`
+                        button.addEventListener('click', () => { elem.firstElementChild.click() })
+                    }
+                    elem.setAttribute('title', "Er kon geen periodenummer worden gedetecteerd.")
+                    break
+
+                default:
+                    if (settingGrid) {
+                        let button = document.createElement('button')
+                        elementGrid.appendChild(button)
+                        button.classList.add('st-sw-item', 'st-obsolete')
+                        button.innerHTML = `<h3>${subject}</h3><p style="font-weight:bold">periode ${period}</p><p style="font-weight:normal;font-size:10px;color:#808080">${title}</p>`
+                        button.addEventListener('click', () => { elem.firstElementChild.click() })
+                    }
+                    elem.classList.add('st-obsolete')
+                    elem.setAttribute('title', `Deze studiewijzer is van periode ${period}.`)
+                    break
             }
-            return { element: elem, period: period }
         })
     }
 }
@@ -140,7 +194,7 @@ async function popstate() {
 }
 
 async function applyStyles() {
-    createStyle(`.st-current:not(:hover){background-color:#f0f8ff!important}.st-current{font-weight:700}.st-obsolete:not(:hover){background-color:#fff0f5!important}.st-obsolete{font-style:italic}.st-current-sw>div>h3,.st-current-sw>div>h3>b,.st-current-sw>div>div>footer.endlink{background:aliceBlue;font-weight:700}`, 'study-tools')
+    createStyle(`.st-current{background-color:#f0f8ff!important;font-weight:700}.st-obsolete{background-color:#fff0f5!important;font-style:italic}.st-current-sw>div>h3,.st-current-sw>div>h3>b,.st-current-sw>div>div>footer.endlink{background:aliceBlue;font-weight:700}.st-sw-grid{display:grid;grid-template-columns:repeat(auto-fit,minmax(16em,1fr));gap:1em}.st-sw-item{background-color:#fafafa;cursor:pointer;display:grid;grid-auto-rows:1fr 1.5em 24px;align-items:center;gap:.3em;padding:1em .5em .3em;aspect-ratio:1/0.7;border-radius:5px;border:1px solid #ccc}.st-current:hover,.st-obsolete:hover,.st-sw-item:hover{filter:brightness(.9)}.st-sw-item h3{overflow:hidden}`, 'study-tools')
 
     if (await getSetting('magister-cf-failred')) {
         createStyle(`.grade[title="5,0"],.grade[title="5,1"],.grade[title="5,2"],.grade[title="5,3"],.grade[title="5,4"],.grade[title^="1,"],.grade[title^="2,"],.grade[title^="3,"],.grade[title^="4,"]{background-color:lavenderBlush !important;color:red !important;font-weight:700}`, 'study-tools-cf-failred')
@@ -156,10 +210,11 @@ function getWeekNumber() {
 }
 
 function getPeriodNumber(w) {
-    if (w >= 30 && w < 48)
+    if (!w) w = getWeekNumber()
+    if (w >= 30 && w < 47)
         return 1
 
-    if (w >= 48 || w < 4)
+    if (w >= 47 || w < 4)
         return 2
 
     if (w >= 4 && w < 14)
--- popup.js
+++ popup.js
@@ -1,16 +1,54 @@
+const defaults = {
+    'magister-sw-subjects': `Aardrijkskunde, ak
+Bedrijfseconomie, beco
+Beeldende vorming, be, bv, kube, kubv
+Biologie, bi
+Cult. en kunstz. vorming, ckv
+Drama, dr, kudr
+Duitse taal, dutl, du
+Economie, ec
+Engelse taal, entl, en
+Franse taal, fatl, fa
+Geschiedenis, gs
+Kunst, ku, kua
+Levensbeschouwing, lv
+Maatschappijleer, ma, malv
+Mentor, mentoruur, mentoraat
+Muziek, mu, kumu
+Natuurkunde, na
+Nederlandse taal, netl, ne
+Scheikunde, sk
+Wiskunde, wi
+Wiskunde A, wa
+Wiskunde B, wb
+Wiskunde C, wc
+Wiskunde D, wd`
+}
+
 setSetting('openedPopup', true)
 
 // Bind inputs
 document.querySelectorAll('.bind').forEach(async element => {
     let value = await getSetting(element.id)
-    switch (element.getAttribute('type')) {
+    switch (element.tagName === 'INPUT' ? element.getAttribute('type') : element.tagName) {
         case 'checkbox':
             element.checked = value
             element.addEventListener('input', event => setSetting(event.target.id, event.target.checked))
             break
 
+        case 'TEXTAREA':
+            if (element.classList.contains('bind-array-2d')) {
+                if (value) element.value = Object.values(value)[0].map(e => { return e.join(',') }).join('\n')
+                else {
+                    element.value = defaults[element.id] || undefined
+                    setSetting(element.id, { 1: element.value.replace(/ +(?= )/g, '').split('\n').map(e => { return e.split(',') }) })
+                }
+                element.addEventListener('input', event => setSetting(event.target.id, { 1: event.target.value.replace(/ +(?= )/g, '').split('\n').map(e => { return e.split(',') }) }))
+            }
+            break
+
         default:
-            element.value = value
+            if (value) element.value = value
             element.addEventListener('input', event => setSetting(event.target.id, event.target.value))
             break
     }
