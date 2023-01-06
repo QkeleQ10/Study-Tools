f6211775aad5ed8bbe1422f7b4f5818b60c697cb
QkeleQ10 <quinten@althues.nl>
2023-01-06 23:28:57 +0100

    Popup design overhaul

--- popup.js
+++ popup.js
@@ -2,18 +2,18 @@ let start = {},
     diff = {},
     diffTimestamp = 0
 
-setSetting('openedPopup', true)
+if (chrome?.storage) setSetting('openedPopup', true)
 
 init()
 
 async function init() {
-    start = await getSettings(null, null, true)
+    if(chrome?.storage) start = await getSettings(null, null, true)
 
     document.querySelectorAll('.bind-boolean').forEach(element => {
         let value = start[element.id]
         if (element.tagName === 'INPUT' && element.getAttribute('type') === 'checkbox') {
             element.checked = value
-            element.addEventListener('input', event => pushSetting(event.target.id, event.target.checked))
+            element.addEventListener('input', event => pushSetting(event.target.id, event.target.checked, event.target))
         }
     })
 
@@ -21,36 +21,42 @@ async function init() {
         let value = start[element.id] || defaults[element.id]
         if (element.tagName === 'INPUT' && (element.getAttribute('type') === 'text' || element.getAttribute('type') === 'password' || element.getAttribute('type') === 'email')) {
             if (value) element.value = value
-            element.addEventListener('input', event => pushSetting(event.target.id, event.target.value))
+            element.addEventListener('input', event => pushSetting(event.target.id, event.target.value, event.target))
         }
-        if (!start[element.id] && defaults[element.id]) pushSetting(element.id, defaults[element.id])
+        if (!start[element.id] && defaults[element.id]) pushSetting(element.id, defaults[element.id], element)
     })
 
     document.querySelectorAll('.bind-subjects').forEach(element => {
         let values = start[element.id] || defaults[element.id]
         for (let i = 0; i < values.length + 1; i++) {
-            const value = values[i] || { name: '', aliases: '', color: '#cccccc' }
-            element.innerHTML += `<div class="grid-subjects"><input type="text" value="${value.name}"><input type="text" value="${value.aliases}"><div class="input-color-container"><input type="color" value="${value.color}"></div></div>`
+            const value = values[i] || { name: '', aliases: '' }
+            element.innerHTML += `<div class="grid-subjects"><input type="text" value="${value.name}"><input type="text" value="${value.aliases}"></div>`
         }
         element.querySelectorAll('input').forEach(inputElement => inputElement.addEventListener('input', updateSubjects))
         if (!start[element.id] && defaults[element.id]) updateSubjects()
     })
 
-    if (!chrome.runtime.getManifest().update_url) {
+    if (!chrome?.runtime?.getManifest().update_url) {
         document.querySelectorAll('.if-no-update-url').forEach(e => e.removeAttribute('style'))
     }
+
+    document.querySelectorAll('#sectionPicker>div[data-section]').forEach(element => {
+        element.addEventListener('click', openSection)
+    })
+
+    document.querySelector('header').addEventListener('click', closeSection)
 
     refreshConditionals()
 
     setInterval(async () => {
-        if (new Date().getTime() - diffTimestamp < 500) return
-        document.documentElement.dataset.saved = 'neutral'
+        document.querySelectorAll('[data-saved="saved"]').forEach(e => e.removeAttribute('data-saved'))
+        if (new Date().getTime() - diffTimestamp < 750) return
         if (Object.keys(diff).length === 0) return
         await setSettings(diff, 'sync')
         diff = {}
         diffTimestamp = 0
-        document.documentElement.dataset.saved = 'saved'
-    }, 1000)
+        document.querySelectorAll('[data-saved="not-saved"]').forEach(e => e.setAttribute('data-saved', 'saved'))
+    }, 500)
 }
 
 function updateSubjects() {
@@ -60,8 +66,7 @@ function updateSubjects() {
     subjectWrappers.forEach(subjectWrapper => {
         const subjectValue = {
             name: subjectWrapper.getElementsByTagName('input')[0].value,
-            aliases: subjectWrapper.getElementsByTagName('input')[1].value,
-            color: subjectWrapper.getElementsByTagName('input')[2].value
+            aliases: subjectWrapper.getElementsByTagName('input')[1].value
         },
             empty = !subjectValue.name && !subjectValue.aliases,
             lastChild = !subjectWrapper.nextSibling
@@ -69,7 +74,7 @@ function updateSubjects() {
         if (!empty && lastChild) {
             const newSubjectWrapper = document.createElement('div')
             newSubjectWrapper.classList.add('grid-subjects')
-            newSubjectWrapper.innerHTML = `<input type="text"><input type="text"><div class="input-color-container"><input type="color" value="#cccccc"></div>`
+            newSubjectWrapper.innerHTML = `<input type="text"><input type="text">`
             newSubjectWrapper.querySelectorAll('input').forEach(inputElement => inputElement.addEventListener('input', updateSubjects))
             parent.appendChild(newSubjectWrapper)
         }
@@ -79,12 +84,13 @@ function updateSubjects() {
         }
 
     })
-    pushSetting(parent.id, subjectValues)
+    pushSetting(parent.id, subjectValues, parent)
 }
 
-function pushSetting(key, value) {
+function pushSetting(key, value, element) {
+    if (!chrome?.storage) return
     refreshConditionals()
-    document.documentElement.dataset.saved = 'not-saved'
+    if (element) element.parentElement.setAttribute('data-saved', 'not-saved')
     diff[key] = value
     diffTimestamp = new Date().getTime()
     if (String(value).toLowerCase().includes('lgbt')) document.querySelector('header').classList.add('lgbt')
@@ -112,6 +118,20 @@ function refreshConditionals() {
             }
         }
     })
+}
+
+function openSection(event) {
+    let targetSection = document.querySelector(`section[data-title="${event.currentTarget.dataset.section}"]`) || document.querySelector(`section[data-title="Algemeen"]`)
+    document.querySelectorAll('section.open').forEach(e => e.classList.remove('open'))
+    targetSection.classList.add('open')
+    document.getElementById('sectionName').innerText = event.currentTarget.dataset.section || 'Algemeen'
+    document.documentElement.setAttribute('data-section', event.currentTarget.dataset.section || 'Algemeen')
+}
+
+function closeSection(event) {
+    document.querySelectorAll('section.open').forEach(e => e.classList.remove('open'))
+    document.getElementById('sectionName').innerText = 'Configuratiepaneel'
+    document.documentElement.removeAttribute('data-section')
 }
 
 function getSetting(key, location) {
