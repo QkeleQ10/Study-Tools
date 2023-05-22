1abd92e8852d599084197d44f711654f6b89036b
QkeleQ10 <quinten@althues.nl>
2023-05-22 10:01:27 +0200

    Switch from callback to await

--- src/magister/scripts/gamification.js
+++ src/magister/scripts/gamification.js
@@ -34,129 +34,130 @@ async function gamification() {
     //     class: 'st-banner-ad', innerText: "Gamificatie (het berekenen van scores en niveaus) is onderdeel van Study Tools voor Magister.", onclick: `window.open('https://qkeleq10.github.io/extensions/studytools', '_blank').focus()`
     // })
 
-    levelElem.addEventListener('click', ()=>{gmOverlay.showModal()})
-    gmClose.addEventListener('click', ()=>{gmOverlay.close()})
+    levelElem.addEventListener('click', () => { gmOverlay.showModal() })
+    gmClose.addEventListener('click', () => { gmOverlay.close() })
 
     calculateScore()
 
     async function calculateScore() {
-        chrome.runtime.sendMessage(['token', 'userId'], async response => {
-            const token = response.token,
-                userId = response.userId,
-                hostname = response.hostname
+        let response = await chrome.runtime.sendMessage(['token', 'userId']),
+            token = response.token,
+            userId = response.userId,
+            hostname = response.hostname
 
-            // Fetch all years and info related.
-            const yearsRes = await fetch(`${hostname}/api/leerlingen/${userId}/aanmeldingen?begin=2013-01-01&einde=${new Date().getFullYear() + 1}-01-01`, { headers: { Authorization: token } }),
-                yearsArray = (await yearsRes.json()).items,
-                years = {}
+        console.log(response)
 
-            // Loop through each year and gather grades, absences and assignments. Bind them to their respective key in the 'years' object.
-            yearsArray.forEach(async year => {
-                const gradesRes = await fetch(`${hostname}/api/personen/${userId}/aanmeldingen/${year.id}/cijfers/cijferoverzichtvooraanmelding?actievePerioden=false&alleenBerekendeKolommen=false&alleenPTAKolommen=false&peildatum=${year.einde}`, { headers: { Authorization: token } })
-                const gradesJson = (await gradesRes.json()).Items
+        // Fetch all years and info related.
+        const yearsRes = await fetch(`${hostname}/api/leerlingen/${userId}/aanmeldingen?begin=2013-01-01&einde=${new Date().getFullYear() + 1}-01-01`, { headers: { Authorization: token } }),
+            yearsArray = (await yearsRes.json()).items,
+            years = {}
 
-                const absencesRes = await fetch(`${hostname}/api/personen/${userId}/absenties?van=${year.begin}&tot=${year.einde}`, { headers: { Authorization: token } }),
-                    absencesJson = (await absencesRes.json()).Items
+        // Loop through each year and gather grades, absences and assignments. Bind them to their respective key in the 'years' object.
+        yearsArray.forEach(async year => {
+            const gradesRes = await fetch(`${hostname}/api/personen/${userId}/aanmeldingen/${year.id}/cijfers/cijferoverzichtvooraanmelding?actievePerioden=false&alleenBerekendeKolommen=false&alleenPTAKolommen=false&peildatum=${year.einde}`, { headers: { Authorization: token } })
+            const gradesJson = (await gradesRes.json()).Items
 
-                const assignmentsRes = await fetch(`${hostname}/api/personen/${userId}/opdrachten?top=250&startdatum=${year.begin}&einddatum=${year.einde}`, { headers: { Authorization: token } }),
-                    assignmentsJson = (await assignmentsRes.json()).Items
+            const absencesRes = await fetch(`${hostname}/api/personen/${userId}/absenties?van=${year.begin}&tot=${year.einde}`, { headers: { Authorization: token } }),
+                absencesJson = (await absencesRes.json()).Items
 
-                years[year.id] = { grades: gradesJson, absences: absencesJson, assignments: assignmentsJson, name: year.studie.code }
+            const assignmentsRes = await fetch(`${hostname}/api/personen/${userId}/opdrachten?top=250&startdatum=${year.begin}&einddatum=${year.einde}`, { headers: { Authorization: token } }),
+                assignmentsJson = (await assignmentsRes.json()).Items
+
+            years[year.id] = { grades: gradesJson, absences: absencesJson, assignments: assignmentsJson, name: year.studie.code }
+        })
+
+        // Wait for the requests to finish, then continue calculating the points.
+        checkRequestsDone()
+        function checkRequestsDone() {
+            if (Object.keys(years).length !== yearsArray.length) {
+                setTimeout(checkRequestsDone, 100)
+            } else {
+                let points = calculatePoints(years)
+                displayScore(points)
+            }
+        }
+
+        // Commence calculating the points.
+        function calculatePoints(years) {
+            // TODO: Balancing!
+            // TODO: Points are currently allowed to decrease (when absences are added). Should this also occur when the grade mean drops or when assignments are handed in late?
+            // HANDING IN LATE: Currently commented out due to issues. Assignments that are not mandatory would still deduct points.
+            // ABSENCES: Should licit absences (loopbaanbegeleiding, interne activiteit) be rewarded? Currently, only illicit absences are taken into account and deduct points.
+
+            let points = {
+                absences: {},
+                grades: {},
+                assignmentsEarly: {}
+                // assignmentsLate: {}
+            }
+
+            Object.keys(years).forEach((yearId, i, a) => {
+                let yearName = years[yearId].name,
+                    gradesN = 0,
+                    gradesV = 0,
+                    absencesN = 0,
+                    absencesV = 0,
+                    assignmentsEarlyN = 0,
+                    assignmentsEarlyV = 0
+                // assignmentsLateN = 0,
+                // assignmentsLateV = 0
+
+                // Sufficient grades grant points proportionally to the user's score
+                years[yearId].grades.filter(e => !Number.isNaN(Number(e.CijferStr?.replace(',', '.'))) && e.CijferKolom.KolomSoort === 1).forEach(grade => {
+                    let result = Number(grade.CijferStr?.replace(',', '.'))
+                    if (result < 5.5) return // This prevents deducting points if an insufficient grade is added
+                    gradesN++
+                    gradesV += (3 * result - 12)
+                })
+                gradesV = Math.ceil(gradesV * (0.25 * i + .5))
+                points.grades[yearId] = { n: gradesN, v: gradesV, g: yearName }
+
+                // Illicit absences deduct 8 pt from the user's score
+                years[yearId].absences.filter(e => !e.Geoorloofd).forEach(absence => {
+                    absencesN++
+                    absencesV -= 8
+                })
+                points.absences[yearId] = { n: absencesN, v: absencesV, g: yearName }
+
+                // Assignments can either grant or deduct points
+                years[yearId].assignments.filter(e => e.Afgesloten || e.IngeleverdOp || new Date(e.InleverenVoor) < new Date()).forEach(assignment => {
+                    // if (new Date(assignment.InleverenVoor) < new Date() && (!assignment.Afgesloten || !assignment.IngeleverdOp)) {
+                    //     // Deduct 12 pt if the assignment wasn't handed in even after the due date
+                    //     assignmentsLateN++
+                    //     assignmentsLateV -= 12
+                    // } else if (new Date(assignment.IngeleverdOp) > new Date(assignment.InleverenVoor)) {
+                    //     // Deduct at most 12 pt if the assignment was handed in after the due date
+                    //     assignmentsLateN++
+                    //     assignmentsLateV += Math.max(-12, (new Date(assignment.InleverenVoor) - new Date(assignment.IngeleverdOp)) / 43200000) // Deduct 1 pt per 12 h
+                    // } else
+                    if (new Date(assignment.IngeleverdOp) <= new Date(assignment.InleverenVoor)) {
+                        // Grant at most 20 pt if the assignment was handed in before the due date
+                        assignmentsEarlyN++
+                        assignmentsEarlyV += Math.min(20, (new Date(assignment.InleverenVoor) - new Date(assignment.IngeleverdOp)) / 172800000 + 2) // Add 2 pt, plus 1 pt per 24 h
+                    }
+                })
+                // assignmentsLateV = Math.floor(assignmentsLateV)
+                // points.assignmentsLate[yearId] = { n: assignmentsLateN, v: assignmentsLateV, g: yearName }
+                assignmentsEarlyV = Math.ceil(assignmentsEarlyV * (0.25 * i + .5))
+                points.assignmentsEarly[yearId] = { n: assignmentsEarlyN, v: assignmentsEarlyV, g: yearName }
             })
 
-            // Wait for the requests to finish, then continue calculating the points.
-            checkRequestsDone()
-            function checkRequestsDone() {
-                if (Object.keys(years).length !== yearsArray.length) {
-                    setTimeout(checkRequestsDone, 100)
-                } else {
-                    let points = calculatePoints(years)
-                    displayScore(points)
-                }
-            }
-
-            // Commence calculating the points.
-            function calculatePoints(years) {
-                // TODO: Balancing!
-                // TODO: Points are currently allowed to decrease (when absences are added). Should this also occur when the grade mean drops or when assignments are handed in late?
-                // HANDING IN LATE: Currently commented out due to issues. Assignments that are not mandatory would still deduct points.
-                // ABSENCES: Should licit absences (loopbaanbegeleiding, interne activiteit) be rewarded? Currently, only illicit absences are taken into account and deduct points.
-
-                let points = {
-                    absences: {},
-                    grades: {},
-                    assignmentsEarly: {}
-                    // assignmentsLate: {}
-                }
-
-                Object.keys(years).forEach((yearId, i, a) => {
-                    let yearName = years[yearId].name,
-                        gradesN = 0,
-                        gradesV = 0,
-                        absencesN = 0,
-                        absencesV = 0,
-                        assignmentsEarlyN = 0,
-                        assignmentsEarlyV = 0
-                    // assignmentsLateN = 0,
-                    // assignmentsLateV = 0
-
-                    // Sufficient grades grant points proportionally to the user's score
-                    years[yearId].grades.filter(e => !Number.isNaN(Number(e.CijferStr?.replace(',', '.'))) && e.CijferKolom.KolomSoort === 1).forEach(grade => {
-                        let result = Number(grade.CijferStr?.replace(',', '.'))
-                        if (result < 5.5) return // This prevents deducting points if an insufficient grade is added
-                        gradesN++
-                        gradesV += (3 * result - 12)
-                    })
-                    gradesV = Math.ceil(gradesV * (0.25 * i + .5))
-                    points.grades[yearId] = { n: gradesN, v: gradesV, g: yearName }
-
-                    // Illicit absences deduct 8 pt from the user's score
-                    years[yearId].absences.filter(e => !e.Geoorloofd).forEach(absence => {
-                        absencesN++
-                        absencesV -= 8
-                    })
-                    points.absences[yearId] = { n: absencesN, v: absencesV, g: yearName }
-
-                    // Assignments can either grant or deduct points
-                    years[yearId].assignments.filter(e => e.Afgesloten || e.IngeleverdOp || new Date(e.InleverenVoor) < new Date()).forEach(assignment => {
-                        // if (new Date(assignment.InleverenVoor) < new Date() && (!assignment.Afgesloten || !assignment.IngeleverdOp)) {
-                        //     // Deduct 12 pt if the assignment wasn't handed in even after the due date
-                        //     assignmentsLateN++
-                        //     assignmentsLateV -= 12
-                        // } else if (new Date(assignment.IngeleverdOp) > new Date(assignment.InleverenVoor)) {
-                        //     // Deduct at most 12 pt if the assignment was handed in after the due date
-                        //     assignmentsLateN++
-                        //     assignmentsLateV += Math.max(-12, (new Date(assignment.InleverenVoor) - new Date(assignment.IngeleverdOp)) / 43200000) // Deduct 1 pt per 12 h
-                        // } else
-                        if (new Date(assignment.IngeleverdOp) <= new Date(assignment.InleverenVoor)) {
-                            // Grant at most 20 pt if the assignment was handed in before the due date
-                            assignmentsEarlyN++
-                            assignmentsEarlyV += Math.min(20, (new Date(assignment.InleverenVoor) - new Date(assignment.IngeleverdOp)) / 172800000 + 2) // Add 2 pt, plus 1 pt per 24 h
-                        }
-                    })
-                    // assignmentsLateV = Math.floor(assignmentsLateV)
-                    // points.assignmentsLate[yearId] = { n: assignmentsLateN, v: assignmentsLateV, g: yearName }
-                    assignmentsEarlyV = Math.ceil(assignmentsEarlyV * (0.25 * i + .5))
-                    points.assignmentsEarly[yearId] = { n: assignmentsEarlyN, v: assignmentsEarlyV, g: yearName }
+            // All points are added up and returned
+            points.total = 0
+            Object.keys(points).forEach(categoryKey => {
+                if (categoryKey === 'total') return
+                points[categoryKey].sum = 0
+                Object.keys(points[categoryKey]).forEach(yearKey => {
+                    if (yearKey === 'sum') return
+                    points[categoryKey].sum += points[categoryKey][yearKey].v
                 })
+                points.total += points[categoryKey].sum
+            })
 
-                // All points are added up and returned
-                points.total = 0
-                Object.keys(points).forEach(categoryKey => {
-                    if (categoryKey === 'total') return
-                    points[categoryKey].sum = 0
-                    Object.keys(points[categoryKey]).forEach(yearKey => {
-                        if (yearKey === 'sum') return
-                        points[categoryKey].sum += points[categoryKey][yearKey].v
-                    })
-                    points.total += points[categoryKey].sum
-                })
+            if (points.total < 0) points.total = 0
 
-                if (points.total < 0) points.total = 0
-
-                return points
-            }
-        })
+            return points
+        }
     }
 
     async function displayScore(points) {
